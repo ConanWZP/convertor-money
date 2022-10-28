6086b0321dcb68ced7557d527abd2eebba4dc536
ConanWZP <yaroslav2281337@mail.ru>
2022-10-28 22:16:29 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './styles.scss'
+import Block from './components/Block';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [rates, setRates] = useState<any>({})
+
+    const [fromCurrency, setFromCurrency] = useState('RUB')
+    const [toCurrency, setToCurrency] = useState('USD')
+    const [fromPrice, setFromPrice] = useState("")
+    const [toPrice, setToPrice] = useState("")
+
+
+
+    const getCurrencies = async () => {
+        await axios.get('https://cdn.cur.su/api/latest.json')
+            .then((res) => {
+                setRates(res.data.rates)
+
+                console.log(typeof res.data.rates)
+        })
+    }
+
+
+    useEffect(() => {
+        getCurrencies()
+    }, [])
+
+    const onChangeFromPrice = (value: string) => {
+        const price = Number(value) / rates[fromCurrency]
+        const conclusionPrice = price * rates[toCurrency]
+        setToPrice(String(conclusionPrice))
+        setFromPrice(value)
+    }
+
+    const onChangeToPrice = (value: string) => {
+        const price = Number(value) / rates[toCurrency]
+        const conclusionPrice = price * rates[fromCurrency]
+        setFromPrice(String(conclusionPrice))
+        setToPrice(value)
+    }
+
+    return (
+        <div className="App">
+          <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice} />
+          <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice} />
+        </div>
+    );
+};
 
 export default App;
